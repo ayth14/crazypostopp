0c241d798be8c28656bbcbe47453cb529c60dc4c
Ayush Prashant <ayushthunder14@gmail.com>
2022-11-17 23:18:55 +0530

    Related Task(s): installed tailwind and updated the npm

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,22 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" type="image/jpg" href="{{ asset('favicon.ico') }}" />
-
+    <link rel="stylesheet" href="{{ URL::to('assets/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('assets/css/output.css') }}">
     {{-- <link rel="icon" href="images/favicon.ico" /> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        laravel: "#ef3b2d",
-                    },
-                },
-            },
-        };
-    </script>
     <title>CrazyPostOpp | Find Laravel Jobs & Projects</title>
 </head>
 
